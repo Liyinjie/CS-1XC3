da0c4ad58a6e10a67c826144d426f80bc50baedd
Liyinjie <123437085+Liyinjie@users.noreply.github.com>
2023-03-10 16:34:23 -0500

    Add files via upload

---
+++ L06/part4/Lab6Part4.h
@@ -0,0 +1,8 @@
+int sum2(int x, int y);
+int sum3(int x, int y);
+int sum4(int x, int y);
+int prod2(int x, int y);
+int prod3(int x, int y);
+int prod4(int x, int y);
+void barrettsPrivateers (void);
+int main (void);
